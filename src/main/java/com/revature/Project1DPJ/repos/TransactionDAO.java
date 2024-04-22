0cd0067b9ee3cbcad346b26e2a7fb913aeaff188
Jonathan Pierre <JonWPierre@gmail.com>
2024-04-22 15:50:15 -0400

    Added JPA REpository

--- src/main/java/com/revature/Project1DPJ/repos/TransactionDAO.java
+++ src/main/java/com/revature/Project1DPJ/repos/TransactionDAO.java
@@ -1,4 +1,8 @@
 package com.revature.Project1DPJ.repos;
 
-public interface TransactionDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionDAO extends JpaRepository {
 }
