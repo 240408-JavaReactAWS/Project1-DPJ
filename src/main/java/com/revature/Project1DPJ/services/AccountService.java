96bac2ba307568266b4b78e7b56916739b61c365
Donald Keita <keitadonald>
2024-04-24 03:21:16 -0400

    added some functions

--- src/main/java/com/revature/Project1DPJ/services/AccountService.java
+++ src/main/java/com/revature/Project1DPJ/services/AccountService.java
@@ -1,9 +1,12 @@
 package com.revature.Project1DPJ.services;
 
 import com.revature.Project1DPJ.models.Account;
+import com.revature.Project1DPJ.models.User;
 import com.revature.Project1DPJ.repos.AccountDAO;
+import com.revature.Project1DPJ.repos.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -12,10 +15,12 @@ import java.util.List;
 public class AccountService {
 
     private AccountDAO accountDAO;
+    private UserDAO userDAO;
 
     @Autowired
-    public AccountService(AccountDAO accountDAO) {
+    public AccountService(AccountDAO accountDAO, UserDAO userDAO) {
         this.accountDAO = accountDAO;
+        this.userDAO = userDAO;
     }
 
 
@@ -23,4 +28,42 @@ public class AccountService {
         return this.accountDAO.findAll();
     }
 
+
+    public Account getUserAccountById(int id) {
+        Optional<Account> optionalAccount = this.accountDAO.findById(id);
+        return optionalAccount.orElse(null);
+    }
+
+
+
+    public boolean deleteUserAccountById(int id) {
+
+        if (!this.accountDAO.existsById(id)) return true;
+
+        this.accountDAO.deleteById(id);
+
+        // if account object exists in the database, return false. Otherwise, return true
+        return !this.accountDAO.existsById(id);
+    }
+
+
+    public boolean resetUserAccountPassword(int id, String password) {
+        Optional<Account> optionalAccount = this.accountDAO.findById(id);
+        if (optionalAccount.isPresent()) {
+            Account account = optionalAccount.get();
+            int userId = account.getAccountOwnerId();
+
+            Optional<User> optionalUser = this.userDAO.findById(userId);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                user.setPassword(password);
+                this.userDAO.save(user);
+                return this.userDAO.existsById(user.getId());
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
